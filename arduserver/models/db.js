a2a1e02feb72836ec8d8bff3a763444ad7fd59f5
Victor Usov <vic-u@live.com>
2025-06-15 18:44:26 +0300

    entry and sensor

--- arduserver/models/db.js
+++ arduserver/models/db.js
@@ -7,32 +7,113 @@ const cmdtbl2 = 'commanddata2';
 const usrtbl = 'userdata';
 
 db.serialize(() => {
-    //var sql = `CREATE TABLE IF NOT EXISTS ${snstbl} (id integer primary key, macaddress, sensorname, sensorvalue, year, month, day, hour)`;
-    //db.run(`DROP TABLE IF EXISTS ${snstbl2}`, err => {
-    //    if (err) console.log('db.serialize err: ' + err);
-    //});
-    //sql = `CREATE TABLE IF NOT EXISTS ${snstbl} (id integer primary key, mac, name, value, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)`;
-    sql = `CREATE TABLE IF NOT EXISTS ${snstbl} (id integer primary key, mac text, name text, value text, timestamp DATETIME DEFAULT (datetime('now', 'localtime')))`;
+       let sql = `CREATE TABLE IF NOT EXISTS ${snstbl}
+    (
+        id
+        integer
+        primary
+        key,
+        mac
+        text,
+        name
+        text,
+        value
+        text,
+        timestamp
+        DATETIME
+        DEFAULT (
+        datetime
+               (
+        'now',
+        'localtime'
+               )))`
     db.run(sql, err => {
         if (err) console.log('db.serialize err: ' + err);
     });
-    sql = `CREATE TABLE IF NOT EXISTS ${snstbl2} (id integer primary key, mac text, name text, box_value text, room_value text, timestamp DATETIME DEFAULT (datetime('now', 'localtime')))`;
+    sql = `CREATE TABLE IF NOT EXISTS ${snstbl2}
+    (
+        id
+        integer
+        primary
+        key,
+        mac
+        text,
+        name
+        text,
+        box_value
+        text,
+        room_value
+        text,
+        timestamp
+        DATETIME
+        DEFAULT (
+        datetime
+           (
+        'now',
+        'localtime'
+           )))`;
     db.run(sql, err => {
         if (err) console.log('db.serialize err: ' + err);
     });
     //db.run(`DROP TABLE IF EXISTS ${cmdtbl}`, err => {
     //    if (err) console.log('db.serialize err: ' + err);
     //});
-    sql = `CREATE TABLE IF NOT EXISTS ${cmdtbl} (id integer primary key, mac, name, turn, temp, delta, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)`;
+    sql = `CREATE TABLE IF NOT EXISTS ${cmdtbl}
+           (
+               id
+               integer
+               primary
+               key,
+               mac,
+               name,
+               turn,
+               temp,
+               delta,
+               timestamp
+               DATETIME
+               DEFAULT
+               CURRENT_TIMESTAMP
+           )`;
     db.run(sql, err => {
-        if(err) console.log('db.serialize err: ' + err);
+        if (err) console.log('db.serialize err: ' + err);
     });
-    sql = `CREATE TABLE IF NOT EXISTS ${cmdtbl2} (id integer primary key, mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)`;
+    sql = `CREATE TABLE IF NOT EXISTS ${cmdtbl2}
+           (
+               id
+               integer
+               primary
+               key,
+               mac,
+               name,
+               turn_heater,
+               turn_holl,
+               turn_water,
+               turn_irr,
+               temp,
+               delta,
+               timestamp
+               DATETIME
+               DEFAULT
+               CURRENT_TIMESTAMP
+           )`;
     db.run(sql, err => {
         if (err) console.log('db.serialize err: ' + err);
     });
 
-    sql = `CREATE TABLE IF NOT EXISTS ${usrtbl} (id integer primary key, mail, phone, mac, pass, salt, unid  TEXT)`;
+    sql = `CREATE TABLE IF NOT EXISTS ${usrtbl}
+           (
+               id
+               integer
+               primary
+               key,
+               mail,
+               phone,
+               mac,
+               pass,
+               salt,
+               unid
+               TEXT
+           )`;
     db.run(sql, err => {
         if (err) console.log('db.serialize err: ' + err);
     });
@@ -43,42 +124,46 @@ db.serialize(() => {
 });
 
 class DBSensor {
-    static getSensorData(data, cb) {
-        var date = new Date();
-        var y = date.getFullYear();
-        var m = date.getMonth() + 1;
-        var d = date.getDate();
-        var h = date.getHours();
+    static getSensorDataForLastHour(data, cb) {
+        const date = new Date();
+        const y = date.getFullYear();
+        let m = date.getMonth() + 1;
+        let d = date.getDate();
+        let h = date.getHours();
 
         d = (d < 10) ? '0' + d : d;
         m = (m < 10) ? '0' + m : m;
         h = (h < 10) ? '0' + h : h;
 
-        var dt = y + '-' + m + '-' + d + '-' + h;
+        const dt = y + '-' + m + '-' + d + '-' + h;
 
         db.get(`SELECT * FROM ${snstbl} WHERE strftime('%Y-%m-%d-%H', timestamp) = ? AND mac = ?`, dt, data.mac, cb);
     }
-    static getSensorData2(data, cb) {
-        var date = new Date();
-        var y = date.getFullYear();
-        var m = date.getMonth() + 1;
-        var d = date.getDate();
-        var h = date.getHours();
+
+    static getSensorDataForLastHour2(data, cb) {
+        const date = new Date()
+        const y = date.getFullYear()
+        let m = date.getMonth() + 1
+        let d = date.getDate()
+        let h = date.getHours()
 
         d = (d < 10) ? '0' + d : d;
         m = (m < 10) ? '0' + m : m;
         h = (h < 10) ? '0' + h : h;
 
-        var dt = y + '-' + m + '-' + d + '-' + h;
+        const dt = y + '-' + m + '-' + d + '-' + h;
 
         db.get(`SELECT * FROM ${snstbl2} WHERE strftime('%Y-%m-%d-%H', timestamp) = ? AND mac = ?`, dt, data.mac, cb);
     }
+
     static getSensorDataByMAC(mac, cb) {
         db.all(`SELECT id, mac, name, value,  strftime('%Y-%m-%d-%H', timestamp) as timestamp  FROM ${snstbl} WHERE mac = ? ORDER BY timestamp DESC`, mac, cb);
     }
+
     static getSensorDataByMAC2(mac, cb) {
         db.all(`SELECT id, mac, name, box_value,  room_value, strftime('%Y-%m-%d-%H', timestamp) as timestamp  FROM ${snstbl2} WHERE mac = ? ORDER BY timestamp DESC`, mac, cb);
     }
+
     static getSensorDataByMAC2ForYear(mac, cb) {
         db.all(`SELECT id, mac, name, box_value,  room_value, strftime('%Y-%m-%d-%H', timestamp) as timestamp  FROM ${snstbl2} WHERE timestamp >= date('now','-12 months') ORDER BY timestamp DESC`, mac, cb);
     }
@@ -97,6 +182,7 @@ class DBSensor {
             db.all(`SELECT id, mac, name, value,  strftime('%Y-%m-%d-%H', timestamp) as timestamp  FROM ${snstbl} WHERE mac = ? AND timestamp >= date('now','-1 year') ORDER BY timestamp DESC`, mac, cb);
         }
     }
+
     static getSensorDataByMacAndDate2(mac, dtype, cb) {
         if (dtype === "d") {
             db.all(`SELECT id, mac, name, box_value,  room_value,  strftime('%Y-%m-%d-%H', timestamp) as timestamp  FROM ${snstbl2} WHERE mac = ? AND timestamp >= date('now','-1 day') ORDER BY timestamp DESC`, mac, cb);
@@ -112,52 +198,78 @@ class DBSensor {
         }
     }
 
-    static setNewData(data, cb) {
-        const sql = `INSERT INTO ${snstbl}(mac, name, value) VALUES (?, ?, ?)`;
-        db.run(sql, data.mac, data.name, data.value, cb);
+    /***получаем данные с датчика и сохраняем, если за текущий час еще не было данных**/
+    static setNewDataForLastHour(data, cb) {
+        this.getSensorDataForLastHour({mac: data.mac}, (err, lastData) => {
+            if (err) return cb(err)
+            //console.log('Fetching:2.1')
+            if (lastData !== undefined) return cb()//не пишем данные за текущий час
+            //console.log('Fetching:2.2')
+            const sql = `INSERT INTO ${snstbl}(mac, name, value)
+                         VALUES (?, ?, ?)`
+            db.run(sql, data.mac, data.name, data.value, cb);
+        })
     }
-    static setNewData2(data, cb) {
-        const sql = `INSERT INTO ${snstbl2}(mac, name, box_value, room_value) VALUES (?, ?, ?, ?)`;
-        db.run(sql, data.mac, data.name, data.boxValue, data.roomValue, cb);
+
+    static setNewDataForLastHour2(data, cb) {
+        this.getSensorDataForLastHour2({mac: data.mac}, (err, lastData) => {
+            if (err) return cb(err)
+            console.log('Fetching2: 1')
+            if (lastData !== undefined) return cb()
+            console.log('Fetching2: 2')
+            const sql = `INSERT INTO ${snstbl2}(mac, name, box_value, room_value)
+                         VALUES (?, ?, ?, ?)`
+            db.run(sql, data.mac, data.name, data.boxValue, data.roomValue, cb);
+        })
     }
+
     static getLastCommand(mac, cb) {
         db.get(`SELECT * FROM ${cmdtbl} WHERE mac = ? ORDER BY id DESC LIMIT 1;`, mac, cb);
     }
+
     static getLastCommand2(mac, cb) {
         db.get(`SELECT * FROM ${cmdtbl2} WHERE mac = ? ORDER BY id DESC LIMIT 1;`, mac, cb);
     }
+
     static setCommand(data, cb) {
         if (data.turn === '') {
+            //console.log('Command: 1')
             return cb();
         }
-        const sql = `INSERT INTO ${cmdtbl}( mac, name, turn, temp, delta) VALUES (?,?,?,?,?)`;
+        //console.log('Command: 2')
+        const sql = `INSERT INTO ${cmdtbl}(mac, name, turn, temp, delta)
+                     VALUES (?, ?, ?, ?, ?)`;
         db.run(sql, data.mac, data.name, data.turn, data.temp, data.delta, cb);
     }
+
     static setCommand2(data, cb) {
-        console.log('set command 2')
+        //console.log('set command 2')
         this.getLastCommand2(data.mac, (err, entry) => {
-            console.log(JSON.stringify(entry))
-            console.log(JSON.stringify(data))
+            // console.log(JSON.stringify(entry))
+            // console.log(JSON.stringify(data))
             //просто команда с модема
             if (data.heaterTurn === '' && data.hollTurn === '' && data.waterTurn === '' && data.irrTurn === '' && data.temp === '' && data.delta === '') return cb()
 
             if (data.heaterTurn === '' && data.hollTurn === '' && data.waterTurn === '' && data.irrTurn === '' && data.temp === '' && data.delta !== '') {
-                const sql = `INSERT INTO ${cmdtbl2}( mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
-                db.run(sql, data.mac, data.name, entry.turn_heater, entry.turn_holl, entry.turn_water, entry.turn_irr, data.temp, data.delta, cb)
+                const sql = `INSERT INTO ${cmdtbl2}(mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta)
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+                db.run(sql, data.mac, data.name, entry.turn_heater, entry.turn_holl, entry.turn_water, entry.turn_irr, entry.temp, data.delta, cb)
             } else if (data.heaterTurn === '' && data.hollTurn === '' && data.waterTurn === '' && data.irrTurn === '' && data.temp !== '' && data.delta === '') {
-                const sql = `INSERT INTO ${cmdtbl2}( mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
-                db.run(sql, data.mac, data.name, entry.turn_heater, entry.turn_holl, entry.turn_water, entry.turn_irr, data.temp, data.delta, cb)
+                const sql = `INSERT INTO ${cmdtbl2}(mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta)
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+                db.run(sql, data.mac, data.name, entry.turn_heater, entry.turn_holl, entry.turn_water, entry.turn_irr, data.temp, entry.delta, cb)
             } else if (data.heaterTurn === '' && data.hollTurn === '' && data.waterTurn === '' && data.irrTurn === '' && data.temp !== '' && data.delta !== '') {
-                const sql = `INSERT INTO ${cmdtbl2}( mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+                const sql = `INSERT INTO ${cmdtbl2}(mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta)
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
                 db.run(sql, data.mac, data.name, entry.turn_heater, entry.turn_holl, entry.turn_water, entry.turn_irr, data.temp, data.delta, cb)
             } else {
                 //пришла команда
-                console.log('get modem command')
+               // console.log('get modem command')
                 if (data.heaterTurn === '') data.heaterTurn = 'OFF' // с модема приходит обычная команда, тогда часть пустое, но температуру и дельту могли переключить
                 if (data.hollTurn === '') data.hollTurn = 'OFF'
                 if (data.waterTurn === '') data.waterTurn = 'OFF'
                 if (data.irrTurn === '') data.irrTurn = 'OFF'  //пришла просто команда без изменений, пустая и температуры равны последним установленным
-                console.log(JSON.stringify(data))
+                //console.log(JSON.stringify(data))
                 if (entry !== undefined && //команда приходит, но совпадает
                     entry.turn_heater === data.heaterTurn &&
                     entry.turn_holl === data.hollTurn &&
@@ -165,29 +277,35 @@ class DBSensor {
                     entry.turn_irr === data.irrTurn &&
                     entry.temp === data.temp &&
                     entry.delta === data.delta) return cb()
-                console.log('insert new command')
-                const sql = `INSERT INTO ${cmdtbl2}( mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+                //console.log('insert new command')
+                const sql = `INSERT INTO ${cmdtbl2}(mac, name, turn_heater, turn_holl, turn_water, turn_irr, temp, delta)
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
                 db.run(sql, data.mac, data.name, data.heaterTurn, data.hollTurn, data.waterTurn, data.irrTurn, data.temp, data.delta, cb)
             }
         })
     }
 }
+
 class DBUser {
     static create(data, cb) {
-        const sql = `INSERT INTO ${usrtbl}(mail, phone, pass, salt, mac) VALUES (?,?,?,?,?)`;
+        const sql = `INSERT INTO ${usrtbl}(mail, phone, pass, salt, mac)
+                     VALUES (?, ?, ?, ?, ?)`;
         db.run(sql, data.mail, data.phone, data.pass, data.salt, data.mac, cb);
     }
+
     static findmail(mail, cb) {
         if (!mail) return cb(new Error('Please provide an mail'));
         db.get(`SELECT * FROM ${usrtbl} WHERE mail=?`, mail, cb);
 
     }
+
     static findmac(mac, cb) {
         if (!mac) return cb(new Error('Please provide an mac'));
         db.get(`SELECT * FROM ${usrtbl} WHERE mac=?`, mac, cb);
 
     }
 }
+
 module.exports = db;
 module.exports.DBSensor = DBSensor;
 module.exports.DBUser = DBUser;
--- arduserver/routes/rest.js
+++ arduserver/routes/rest.js
@@ -1,5 +1,4 @@
 const DBSensor = require('../models/db').DBSensor
-const DBUser = require('../models/db').DBUser
 const User = require('../models/user')
 const MAC = '26FD52AD4E93'
 const MAC2 = '26FD52AD4E94'
@@ -7,117 +6,102 @@ const MAC2 = '26FD52AD4E94'
 //rest, который вызывает сенсор, чтобы передать текущие данные и забрать текущие команды
 exports.entry = (req, res, next) => {
     const data = req.params.dt
-    console.log('Fetching:', data)
-    if (data.toString() === '') return next()
-
+    //=26FD52AD4E93=o1s1=24.22==25=2 //arr2 - sensor id
     const arr = data.toString().replace(/\s+/g, '').split("=")
     if (arr.length !== 7) return next('wrong arguments')
     if (arr[1] !== MAC) return next('wrong MAC ADDRESS')
-    DBSensor.getSensorData({mac: MAC}, (err, data) => {
-        if (err) {
-            console.log('getSensorData Err: ' + err)
-            return next(err)
-        }
-        if (data !== undefined) return res.status(200).send('already have') // запись про текущий день и час уже есть
-        DBSensor.setNewData({mac: MAC, name: arr[2], value: arr[3]}, function (err, id) {
-            if (err) {
-                console.log('setNewData Err: ' + err)
-                return next(err)
-            }
+    DBSensor.setNewDataForLastHour({//выставит данные по температуре, только если их не было в течении последнего часа
+            mac: MAC,
+            name: arr[2],
+            value: arr[3]
+        },
+        (err) => {
+            if (err) return next(err)
+            //console.log('Fetching:2')
+            //если команда была дана через модем, то ее тоже выставим
             DBSensor.setCommand({
                 mac: MAC,
                 name: arr[2],
                 turn: arr[4],
                 temp: arr[5],
                 delta: arr[6]
-            }, function (err, data) {
+            }, (err) => {
                 if (err) return next(err)
+                //  console.log('Fetching:3')
                 DBSensor.getLastCommand(MAC, (err, data) => {
                     if (err) return next(err)
                     const turn = (data !== undefined && data.turn === 'ON') ? 'ON' : 'OFF'
                     const result = `=${MAC}=${arr[2]}==${turn}=${data.temp}=${data.delta}`
                     res.status(200).send(result)
-                });
-            });
-        });
-
-    });
-
-
-};
+                })
+            })
+        })
+}
 //rest, который вызывает сенсор, чтобы передать текущие данные и забрать текущие команды
 exports.entry2 = (req, res, next) => {
     const data = req.params.dt;
-    console.log('Fetching:', data)
-    if (data.toString() === '') return next()
-
+    console.log('Fetching2:', data)
+    //=26FD52AD4E94=o2s1=17.80=20.47======
     const arr = data.toString().replace(/\s+/g, '').split("=")
     if (arr.length !== 11) return next('wrong argument')
     if (arr[1] !== MAC2) return next('wrong MAC ADDRESS')
-    DBSensor.getSensorData2({mac: arr[1]}, (err, data) => {
-        if (err) {
-            console.log('getSensorData2 Err: ' + err)
-            return next(err)
-        }
-        if (data !== undefined) return res.status(200).send('already have') // запись про текущий день и час уже есть
-        DBSensor.setNewData2({mac: arr[1], name: arr[2], boxValue: arr[3], roomValue: arr[4]}, (err, id) => {
-            if (err) {
-                console.log('setNewData2 Err: ' + err)
-                return next(err)
-            }
-            DBSensor.setCommand2({
-                mac: arr[1],
-                name: arr[2],
-                heaterTurn: arr[5],
-                hollTurn: arr[6],
-                waterTurn: arr[7],
-                irrTurn: arr[8],
-                temp: arr[9],
-                delta: arr[10]
-            }, function (err, data) {
+    DBSensor.setNewDataForLastHour2({
+        mac: arr[1],
+        name: arr[2],
+        boxValue: arr[3],
+        roomValue: arr[4]
+    }, (err) => {
+        if (err) return next(err)
+        DBSensor.setCommand2({
+            mac: arr[1],
+            name: arr[2],
+            heaterTurn: arr[5],
+            hollTurn: arr[6],
+            waterTurn: arr[7],
+            irrTurn: arr[8],
+            temp: arr[9],
+            delta: arr[10]
+        }, (err, data) =>{
+            if (err) return next(err)
+            DBSensor.getLastCommand2(arr[1], (err, data) => {
                 if (err) return next(err)
-                DBSensor.getLastCommand2(arr[1], (err, data) => {
-                    if (err) return next(err)
-                    heaterResp = (data !== undefined && data.turn_heater === 'ON') ? 'ON' : 'OFF'
-                    hollResp = (data !== undefined && data.turn_holl === 'ON') ? 'ON' : 'OFF'
-                    waterResp = (data !== undefined && data.turn_water === 'ON') ? 'ON' : 'OFF'
-                    irrResp = (data !== undefined && data.turn_irr === 'ON') ? 'ON' : 'OFF'
-                    const result = `=${arr[1]}=${arr[2]}===${heaterResp}=${hollResp}=${waterResp}=${irrResp}=${data.temp}=${data.delta}`
-                    console.log(result)
-                    res.status(200).send(result)
-                })
+                heaterResp = (data !== undefined && data.turn_heater === 'ON') ? 'ON' : 'OFF'
+                hollResp = (data !== undefined && data.turn_holl === 'ON') ? 'ON' : 'OFF'
+                waterResp = (data !== undefined && data.turn_water === 'ON') ? 'ON' : 'OFF'
+                irrResp = (data !== undefined && data.turn_irr === 'ON') ? 'ON' : 'OFF'
+                const result = `=${arr[1]}=${arr[2]}===${heaterResp}=${hollResp}=${waterResp}=${irrResp}=${data.temp}=${data.delta}`
+                res.status(200).send(result)
             })
         })
     })
 }
-//rest. который вызывается при изменении настроек в клиенте для температуры и дельты
+//Rest, который вызывается при изменении настроек в клиенте для температуры и дельты
 exports.sensor = (req, res, next) => {
     console.log('sensor')
     if (req.session.authorized !== true) return next('not authorized')
     User.getByMail(req.session.username, (err, user) => {
         console.log('toggle switch')
-        var turn = 'ON'
-        if (req.body.turn === 'false') turn = 'OFF'//off sensor
+        const turn = req.body.turn === 'false' ? 'OFF' : 'ON'//off sensor
         DBSensor.setCommand({
             mac: user.mac,
             name: 'o1s1',
             turn: turn,
             temp: req.body.temp,
             delta: req.body.delta
-        }, (err, data) => {
+        }, (err) => {
             if (err) return next(err);
+            console.log('sensor is here!' + req.body.turn + "  " + req.body.temp + " " + req.body.delta)
+            next()
         })
     })
-    console.log('sensor is here!' + req.body.turn + "  " + req.body.temp + " " + req.body.delta)
-
-};
-//rest. который вызывается при изменении настроек в клиенте для температуры и дельты
+}
+//Rest, который вызывается при изменении настроек в клиенте для температуры и дельты
 exports.sensor2 = (req, res, next) => {
     if (req.session.authorized !== true) return res.status(403).send('not authorized')
     User.getByMail(req.session.username, (err, user) => {
         if (err) return next(err)
         console.log('toggle switch')
-        console.log(req.body)
+       // console.log(req.body)
         DBSensor.setCommand2({
             mac: MAC2,
             name: 'o2s1',
@@ -127,12 +111,13 @@ exports.sensor2 = (req, res, next) => {
             irrTurn: req.body.irrTurn === 'false' ? 'OFF' : 'ON',
             temp: req.body.temp,
             delta: req.body.delta
-        }, (err, data) => {
+        }, (err) => {
             if (err) return next(err)
+            console.log('sensor is here!' + req.body.temp + ' ' + req.body.delta)
+            res.status(200).send()
         })
     })
-    console.log('sensor is here!' + req.body.temp + ' ' + req.body.delta)
-    res.status(200).send()
+
 }
 //rest. который вызывается при изменении настроек в клиенте для температуры и дельты
 exports.filter = (req, res, next) => {
@@ -145,29 +130,23 @@ exports.filter = (req, res, next) => {
         DBSensor.getSensorDataByMacAndDate(mac, dtype, (err, entries) => {
             if (entries === undefined) entries = [];
 
-            var arr = [];
-            var arr2 = [];
-            var labels = 'labels:[]';
-            var data = 'data: []';
-            var lbl = 'NO DATA';
-            var j = 0;
-            for (i = entries.length - 1; i >= 0; --i) {
-                var e = entries[i];
-                var dt = "'" + e.timestamp + "'";
+            const arr = [];
+            const arr2 = [];
+            let lbl = 'NO DATA';
+            let j = 0;
+            for (let i = entries.length - 1; i >= 0; --i) {
+                const e = entries[i];
+                const dt = "'" + e.timestamp + "'";
                 //arr[j] = dt.toString();
                 arr[j] = e.timestamp;
                 arr2[j] = parseFloat(e.value);
                 lbl = `${e.name} ${e.mac}`;
                 j++;
             }
-            labels = 'labels:' + '[' + arr.toString() + ']';
-            data = 'data:' + '[' + arr2.toString() + ']';
             if (err) return next(err);
             if (entries) {
-                //res.send({ ${label}, ${labels}, ${data} });
                 res.send({label: lbl, labels: arr, data: arr2});
             }
-            ;
         });
     });
 };
